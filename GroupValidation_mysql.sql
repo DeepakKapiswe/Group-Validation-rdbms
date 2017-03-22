bef6913d66cc7deea7f938c0256524f4742a45ce
SupriyaGhangale <supriyaghangale94@gmail.com>
2017-03-23 03:47:33 +0530

    partial editing_2

--- GroupValidation_mysql.sql
+++ GroupValidation_mysql.sql
@@ -11,23 +11,24 @@ reminders be generated on trigger
 begin transaction;
 */
 
-DROP TABLE IF EXISTS aux_userApplicationInfo;
-DROP TABLE IF EXISTS aux_maybeRollNos;
-DROP TABLE IF EXISTS aux_currentGroupMembersGIds;
-DROP TABLE IF EXISTS aux_currentGroupMembersRollNos;
-DROP TABLE IF EXISTS aux_maybeGroupMembers;
-DROP TABLE IF EXISTS aux_maybeGroups;
-DROP TABLE IF EXISTS aux_agreedGroups;
-DROP TABLE IF EXISTS aux_maybeMemberRollNos;
-DROP TABLE IF EXISTS aux_flag2trigger_checkIfGroupMembersAgree;
-DROP TABLE IF EXISTS aux_flag2trigger_updateReminderMsgs;
-DROP TABLE IF EXISTS aux_senderEmails;
-
 DROP TABLE IF EXISTS output_finalValidGroups;
 DROP TABLE IF EXISTS output_conflictingGroups;
 DROP TABLE IF EXISTS output_nonAgreedGroups;
 DROP TABLE IF EXISTS output_lengthExceededGroups;
 DROP TABLE IF EXISTS output_reminderMsgs;
+
+DROP TABLE IF EXISTS aux_userApplicationInfo;
+DROP TABLE IF EXISTS aux_currentGroupMembersGIds;
+DROP TABLE IF EXISTS aux_currentGroupMembersRollNos;
+DROP TABLE IF EXISTS aux_agreedGroups;
+DROP TABLE IF EXISTS aux_maybeGroupMembers;
+DROP TABLE IF EXISTS aux_maybeGroups;
+DROP TABLE IF EXISTS aux_maybeRollNos;
+DROP TABLE IF EXISTS aux_maybeMemberRollNos;
+DROP TABLE IF EXISTS aux_flag2trigger_checkIfGroupMembersAgree;
+DROP TABLE IF EXISTS aux_flag2trigger_updateReminderMsgs;
+DROP TABLE IF EXISTS aux_senderEmails;
+
 
 DROP TABLE IF EXISTS input_userApplications;
 DROP TABLE IF EXISTS input_groupLengthLimit;
@@ -114,12 +115,11 @@ CREATE TABLE aux_maybeGroupMembers
     ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-/*
 CREATE TABLE aux_currentGroupMembersRollNos
 (
-    groupId TEXT NOT NULL,
-    senderRollNo TEXT  NOT NULL,
-    memberRollNo TEXT NOT NULL,
+    groupId INTEGER NOT NULL,
+    senderRollNo VARCHAR (10)  NOT NULL,
+    memberRollNo VARCHAR (10) NOT NULL,
 
     CONSTRAINT PKC_currentGroupMembers PRIMARY KEY (groupId,senderRollNo,memberRollNo),
     FOREIGN KEY (groupId,senderRollNo,memberRollNo) REFERENCES aux_maybeGroupMembers (groupId,senderRollNo,memberRollNo)
@@ -128,8 +128,8 @@ CREATE TABLE aux_currentGroupMembersRollNos
 
 CREATE TABLE aux_currentGroupMembersGIds
 (
-    groupId TEXT NOT NULL,
-    senderRollNo TEXT  NOT NULL,
+    groupId INTEGER NOT NULL,
+    senderRollNo VARCHAR (10)  NOT NULL,
 
     CONSTRAINT PKC_groupId PRIMARY KEY (groupId,senderRollNo),
     FOREIGN KEY (groupId,senderRollNo) REFERENCES aux_maybeGroups (groupId,senderRollNo)
@@ -138,32 +138,32 @@ CREATE TABLE aux_currentGroupMembersGIds
 
 CREATE TABLE aux_agreedGroups
 (
-    finalGId TEXT NOT NULL,
-    groupId TEXT NOT NULL,
-    memberRollNo TEXT NOT NULL,
+    finalGId VARCHAR (255) NOT NULL,
+    groupId INTEGER NOT NULL,
+    memberRollNo VARCHAR (10) NOT NULL,
     CONSTRAINT PKC_aux_agreedGroups PRIMARY KEY (finalGId,groupId,memberRollNo)
 );
 
 CREATE TABLE aux_maybeMemberRollNos
 (
-    memberRollNo TEXT PRIMARY KEY
+    memberRollNo VARCHAR (10) PRIMARY KEY
 );
 
 CREATE TABLE aux_flag2trigger_checkIfGroupMembersAgree
 (
-    val TEXT PRIMARY KEY
+    val VARCHAR (5) PRIMARY KEY
 );
 
 CREATE TABLE aux_flag2trigger_updateReminderMsgs
 (
-    val TEXT PRIMARY KEY
+    val VARCHAR (5) PRIMARY KEY
 );
 
 CREATE TABLE output_finalValidGroups
 (
-    finalGId TEXT NOT NULL,
-    groupId TEXT NOT NULL,
-    memberRollNo TEXT NOT NULL,
+    finalGId VARCHAR (255) NOT NULL,
+    groupId INTEGER NOT NULL,
+    memberRollNo VARCHAR (10) NOT NULL,
 
     CONSTRAINT PKC_output_finalValidGroups PRIMARY KEY (finalGId,groupId,memberRollNo),
     FOREIGN KEY (finalGId,groupId,memberRollNo) REFERENCES aux_agreedGroups (finalGId,groupId,memberRollNo)
@@ -172,8 +172,8 @@ CREATE TABLE output_finalValidGroups
 
 CREATE TABLE output_nonAgreedGroups
 (
-    groupId TEXT NOT NULL,
-    senderRollNo TEXT NOT NULL,
+    groupId INTEGER NOT NULL,
+    senderRollNo VARCHAR (10) NOT NULL,
     CONSTRAINT PKC_output_nonAgreedGroups PRIMARY KEY (groupId,senderRollNo),
     FOREIGN KEY (groupId,senderRollNo) REFERENCES aux_maybeGroups (groupId,senderRollNo)
     ON DELETE RESTRICT ON UPDATE CASCADE
@@ -181,37 +181,39 @@ CREATE TABLE output_nonAgreedGroups
 
 CREATE TABLE output_conflictingGroups
 (
-    finalGId TEXT NOT NULL,
-    groupId TEXT NOT NULL,
-    memberRollNo TEXT NOT NULL,
-    CONSTRAINT PKC_output_conflictingGroups PRIMARY KEY (finalGId,groupId,memberRollNo)
+    finalGId VARCHAR (255) NOT NULL,
+    groupId INTEGER NOT NULL,
+    memberRollNo VARCHAR (10) NOT NULL,
+    CONSTRAINT PKC_output_conflictingGroups PRIMARY KEY (finalGId,groupId,memberRollNo),
     FOREIGN KEY (finalGId,groupId,memberRollNo) REFERENCES aux_agreedGroups (finalGId,groupId,memberRollNo)
     ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE output_lengthExceededGroups
 (
-    finalGId TEXT NOT NULL,
-    groupId TEXT NOT NULL,
-    memberRollNo TEXT NOT NULL,
-    CONSTRAINT PKC_lengthExceededGroup PRIMARY KEY (finalGId,groupId,memberRollNo)
+    finalGId VARCHAR (255) NOT NULL,
+    groupId INTEGER NOT NULL,
+    memberRollNo VARCHAR (10) NOT NULL,
+    CONSTRAINT PKC_lengthExceededGroup PRIMARY KEY (finalGId,groupId,memberRollNo),
     FOREIGN KEY (finalGId,groupId,memberRollNo) REFERENCES aux_agreedGroups (finalGId,groupId,memberRollNo)
     ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE output_reminderMsgs
 (
-    groupId TEXT NOT NULL,
-    senderRollNo TEXT NOT NULL,
-    senderEmailId TEXT NOT NULL,
+    groupId INTEGER NOT NULL,
+    senderRollNo VARCHAR (10) NOT NULL,
+    senderEmailId VARCHAR (255) NOT NULL,
     msg TEXT NOT NULL,
     CONSTRAINT PKC_reminderMsgs PRIMARY KEY (groupId,senderRollno),
     FOREIGN KEY (groupId,senderRollno) REFERENCES aux_maybeGroups (groupId,senderRollno)
     ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
+delimiter |
 
 CREATE TRIGGER trigger_getApplicationInfo after insert on input_userApplications
+FOR EACH ROW
   begin
     delete from aux_maybeRollNos;
     delete from aux_maybeMemberRollNos;
@@ -220,37 +222,55 @@ CREATE TRIGGER trigger_getApplicationInfo after insert on input_userApplications
     delete from aux_flag2trigger_checkIfGroupMembersAgree;
     insert into aux_senderEmails values(new.senderEmail);
     insert into aux_maybeRollNos (remainingMemberRollString) select substr(new.msg,(select instr(new.msg,' ')+1));
-    insert into aux_userApplicationInfo (senderRollNo,senderEmailId,timestamp,validationResult) values ((select substr(new.msg,1,(select instr(new.msg,' ')-1))),new.senderEmail,datetime('now'),'valid');
+    insert into aux_userApplicationInfo (senderRollNo,senderEmailId,timestamp,validationResult) values ((select substr(new.msg,1,(select instr(new.msg,' ')-1))),new.senderEmail,datetime,'valid');
   end;
 
 CREATE TRIGGER trigger_getMaybeRollNos after insert on aux_maybeRollNos
-when (select length(new.remainingMemberRollString)) > 0
+FOR EACH ROW
   begin
-    insert into aux_maybeMemberRollNos  select substr(new.remainingMemberRollString,1,(select instr(new.remainingMemberRollString,' ')-1));
-    insert into aux_maybeRollNos (remainingMemberRollString) select substr(new.remainingMemberRollString,(select instr(new.remainingMemberRollString,' ')+1));
+    if (select length(new.remainingMemberRollString)) > 0
+      then
+      insert into aux_maybeMemberRollNos  select substr(new.remainingMemberRollString,1,(select instr(new.remainingMemberRollString,' ')-1));
+      insert into aux_maybeRollNos (remainingMemberRollString) select substr(new.remainingMemberRollString,(select instr(new.remainingMemberRollString,' ')+1));
+    end if;
   end;
 
 CREATE TRIGGER trigger_insertMaybeGroupDetails after insert on aux_userApplicationInfo
-when new.validationResult='valid'
+FOR EACH ROW
   begin
+    if new.validationResult='valid'
+      then
     insert into aux_maybeGroups values(new.msgId,new.timestamp,new.senderRollNo,new.senderEmailId);
+    end if;
   end;
 
-
+ |
+delimiter ;
+ 
+/*
 CREATE TRIGGER trigger_validateCurrentGroup after insert on aux_maybeGroups
+FOR EACH ROW
 begin
     insert into aux_maybeGroupMembers (groupId,senderRollNo,memberRollNo) select * from (select groupId,senderRollNo from aux_maybeGroups where groupId =new.groupId) inner join aux_maybeMemberRollNos;
+    insert into aux_maybeGroupMembers (groupId,senderRollNo,memberRollNo) select * from (select groupId,senderRollNo from aux_maybeGroups where groupId =new.groupId) left join aux_maybeMemberRollNos;
     insert into aux_currentGroupMembersRollNos select groupId,senderRollNo,memberRollNo from aux_maybeGroupMembers where groupId = new.groupId;
     insert into aux_currentGroupMembersGIds values (new.groupId,new.senderRollNo);
     insert into aux_flag2trigger_checkIfGroupMembersAgree values ('1');
     insert into aux_flag2trigger_updateReminderMsgs values ('1');
 end;
 
+ |
+delimiter ; */
+/*
+
 CREATE TRIGGER trigger_updateGroupMemberAgreementStatus after insert on aux_maybeGroupMembers
-when (select count(*) from aux_maybeGroupMembers where agreementStatus='No' and senderRollNo=new.memberRollNo and memberRollNo=new.senderRollNo) >0
+FOR EACH ROW
 begin
+ if (select count(*) from aux_maybeGroupMembers where agreementStatus='No' and senderRollNo=new.memberRollNo and memberRollNo=new.senderRollNo) >0
+ then
     update aux_maybeGroupMembers set agreementStatus='Yes' where groupId=new.groupId and senderRollNo=new.senderRollNo and memberRollNo=new.memberRollNo;
     update aux_maybeGroupMembers set agreementStatus='Yes' where  senderRollNo=new.memberRollNo and memberRollNo=new.senderRollNo;
+ end if;
 end;
 
 CREATE TRIGGER trigger_insertCurrentGroupMemberGIds after insert on aux_currentGroupMembersRollNos
